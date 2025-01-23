5ecb6139a56bd1e225fdb05b622558f5d32b23f6
bryonk <116611262+bryonk@users.noreply.github.com>
2025-01-23 15:45:59 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Chat");
         System.out.println("What can I do for you?");
@@ -11,13 +13,29 @@ public class Duke {
             String text = scanner.nextLine();
             if (text.equals("bye")) {
                 break;
+            } else if (text.equals("list")) {
+                print_list(list);
+            } else {
+                list.add(text);
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + text);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(text);
-            System.out.println("____________________________________________________________");
         }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void print_list(ArrayList<String> list) {
+        System.out.println("____________________________________________________________");
+        if (list.size() == 0) {
+            System.out.println("list is empty...");
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(i + 1 + ". " + list.get(i));
+            }
+        }
         System.out.println("____________________________________________________________");
     }
 }
