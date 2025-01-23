679573cc8f3781cb36c954f659d679c28dad4cf1
bryonk <116611262+bryonk@users.noreply.github.com>
2025-01-23 15:34:30 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Chat");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(text);
+            System.out.println("____________________________________________________________");
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
