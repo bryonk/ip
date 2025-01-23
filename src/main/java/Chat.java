689cf727a0b4882a5c855d1806c32fd26d0c1a1c
bryonk <116611262+bryonk@users.noreply.github.com>
2025-01-23 17:53:30 +0800

    rename Duke to Chat

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Duke {
+public class Chat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> list = new ArrayList<Task>();
